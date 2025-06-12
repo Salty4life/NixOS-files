18fede38726dc3226cf645d546bcfb91de2be7c5
Salty4life <salty4life.634@proton.me>
2025-06-12 22:22:57 +0100

    Forgor

--- system/configuration.nix
+++ system/configuration.nix
@@ -2,7 +2,6 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {
-  config,
   pkgs,
   inputs,
   self,
@@ -89,9 +88,6 @@
     isNormalUser = true;
     description = "salty";
     extraGroups = ["networkmanager" "wheel"];
-    packages = with pkgs; [
-      #  thunderbird
-    ];
   };
 
   # Install firefox.
