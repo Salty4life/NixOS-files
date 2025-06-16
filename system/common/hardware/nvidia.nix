53b753ebeca6de3a8f03fa6fef67a7e1d2999beb
Salty4life <salty4life.634@proton.me>
2025-06-16 17:33:54 +0100

    hardware: added nvidia specific settings

---
+++ system/common/hardware/nvidia.nix
@@ -0,0 +1,29 @@
+{...}: {
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
+
+    # Enables Nvidia GPU to be used as the primary rendering device
+    prime = {
+      sync.enable = true;
+      nvidiaBusId = "PCI:1@0:0:0";
+      amdgpuBusId = "PCI:4@0:0:0";
+    };
+    open = false;
+
+    nvidiaSettings = true;
+
+    # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    #   version = "555.58.02";
+    #   sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+    #   sha256_aarch64 = lib.fakeSha256;
+    #   openSha256 = lib.fakeSha256;
+    #   settingsSha256 = lib.fakeSha256;
+    #   persistencedSha256 = lib.fakeSha256;
+    # };
+  };
+
+  nixpkgs.config.cudaSupport = true;
+}
