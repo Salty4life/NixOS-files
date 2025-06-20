32102f55d343dc6b1b205b7a6fd037488304df63
Salty4life <salty4life.634@proton.me>
2025-06-20 16:35:15 +0100

    home/graphical: Comments and test commit for mirroring

--- home-manager/common/otd.nix
+++ home-manager/common/otd.nix
@@ -1,8 +1,9 @@
-{pkgs, ...}: let
+{pkgs, ...}:
+# For now these don't really install the plugins, that's a problem for future me anyways
+let
   sources = import ../../_sources/generated.nix {
     inherit (pkgs) fetchgit fetchurl fetchFromGitHub dockerTools;
   };
 in {
-  xdg.configFile."OpenTabletDriver/Plugins/BetterCalibrator.dll".source =
-    "${sources.better-calibrator.src}/BetterCalibrator.dll";
+  xdg.configFile."OpenTabletDriver/Plugins/BetterCalibrator.dll".source = "${sources.better-calibrator.src}/BetterCalibrator.dll";
 }
