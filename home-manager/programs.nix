9b85e09be81220011294409f623dc3f546e56f74
Salty4life <salty4life.634@proton.me>
2025-06-20 11:39:43 +0100

    programs: File Cleanup

--- home-manager/programs.nix
+++ home-manager/programs.nix
@@ -1,3 +1,43 @@
-{...}: {
+{pkgs, ...}: {
   programs.git.enable = true;
+
+  # Add stuff for your user as you see fit:
+  # programs.neovim.enable = true;
+  home.packages = with pkgs; [
+    (discord.override {
+      withMoonlight = true;
+    })
+    (prismlauncher.override {
+      jdks = with pkgs; [
+        temurin-bin
+        openjdk8-bootstrap
+        openjdk17-bootstrap
+      ];
+    })
+    (
+      bottles.override {
+        removeWarningPopup = true;
+      }
+    )
+    davinci-resolve
+    pcsx2
+    rustdesk
+    btop
+    krita
+    alejandra # formatter
+    nixd # nix language server
+    protonmail-desktop
+    protonvpn-gui
+    blender
+    openscad
+    wpsoffice
+    vrcx
+    lutris
+    vlc
+    wget
+    xfce.xfce4-battery-plugin
+    samba
+    r2modman
+    nvfetcher
+  ];
 }
