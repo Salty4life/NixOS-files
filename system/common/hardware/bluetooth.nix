6b6ab7128e09ec04b57250199ec2789b2f330576
Salty4life <salty4life.634@proton.me>
2025-07-02 17:03:01 +0100

    bluetooth: Added bluez and enabled experimental settings

--- system/common/hardware/bluetooth.nix
+++ system/common/hardware/bluetooth.nix
@@ -1,7 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    package = pkgs.bluez;
+    settings = {
+      General = {
+        Experimental = true;
+      };
+    };
   };
   services.blueman.enable = true;
 }
