26faa3223bd7458fbb853d4333f1a49a8c5ae082
Salty4life <salty4life.634@proton.me>
2025-07-03 14:54:57 +0100

    bluetooth: Added xpadneo in order to fix Issue #1

--- system/common/hardware/bluetooth.nix
+++ system/common/hardware/bluetooth.nix
@@ -3,11 +3,18 @@
     enable = true;
     powerOnBoot = true;
     package = pkgs.bluez;
-    settings = {
-      General = {
-        Experimental = true;
-      };
+    settings.General = {
+      Experimental = true; # Enables Battery level readings on Bluetooth devices
+
+      # idk
+      Privacy = "device";
+      JustWorksRepairing = "always";
+      Class = "0x000100";
+      FastConnectable = true;
     };
   };
+
   services.blueman.enable = true;
+
+  hardware.xpadneo.enable = true;
 }
