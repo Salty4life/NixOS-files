4d37d0d5f84dc65387d9590a5ada59e328a54643
Salty4life <salty4life.634@proton.me>
2025-07-03 14:56:25 +0100

    nvidia: Finetuned config to fit needs and added comments for future context

--- system/common/hardware/nvidia.nix
+++ system/common/hardware/nvidia.nix
@@ -1,4 +1,4 @@
-{...}: {
+{config, ...}: {
   # Announces which drivers to enable
   services.xserver.videoDrivers = ["nvidia" "amdgpu"];
 
@@ -7,20 +7,46 @@
 
   # Configures the Nvidia Drivers
   hardware.nvidia = {
+    # Modsetting is required
     modesetting.enable = true;
 
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
     powerManagement.enable = true;
-    #powerManagement.finegrained = true;
 
-    # Enables Nvidia GPU to be used as the primary rendering device
-    prime = {
-      sync.enable = true;
-      nvidiaBusId = "PCI:1@0:0:0";
-      amdgpuBusId = "PCI:4@0:0:0";
-    };
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    # Requires Offload mode to be enabled on Prime
+    powerManagement.finegrained = false;
+
     open = false;
 
     nvidiaSettings = true;
+
+    # Enables Optimus PRIME and it's modes
+    # Double check and stress test your graphic intensive applications before commiting to one configuration
+    # YOU CAN'T USE BOTH SYNC AND OFFLOAD AT THE SAME TIME
+    prime = {
+      # Enables Sync mode (uses the Dedicated Nvidia GPU as the main display device)
+      #sync.enable = true;
+
+      #Enables Offload mode (Uses the integrated GPU as the main display device)
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      # CONFIGURE WITH THE CORRECT BUS ID OTHERWISE SHIT BREAKS!
+      # Use "lshw -c display" to verify
+      # Note that the lshw command displays in hexadecimal, you need to convert to decimal to input it below
+      nvidiaBusId = "PCI:1@0:0:0";
+      amdgpuBusId = "PCI:6@0:0:0";
+    };
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
 
     # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
     #   version = "555.58.02";
