266c170b8fb85d273cca90cdcd5c8d03c9c12c58
Salty4life <salty4life.634@proton.me>
2025-07-10 15:11:05 +0100

    drivers: Added `asusctl` and `solaar` drivers (Fix for #4)

--- system/common/hardware/common.nix
+++ system/common/hardware/common.nix
@@ -1,7 +1,21 @@
-{...}: {
+{pkgs, ...}: {
   # Enables OpenTabletDriver and it's deamon (yes I know it's on the bluetooth file, this is definitely gonna bite me in the ass if I forget to clean it)
   hardware.opentabletdriver = {
     enable = true;
     daemon.enable = true;
+  };
+
+  boot.kernelModules = ["asus-nb-wmi"];
+  environment.systemPackages = with pkgs; [
+    solaar
+    logiops
+    asusctl
+  ];
+
+  hardware.logitech.wireless.enable = true;
+
+  programs.rog-control-center = {
+    enable = true;
+    autoStart = true;
   };
 }
