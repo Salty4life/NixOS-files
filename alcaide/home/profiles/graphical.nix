06444ddc78104df4ca74a470b4cd477cbe5b84af
Salty4life <salty4life.634@proton.me>
2025-08-13 12:10:02 +0100

    profiles: Enabled Steam and game-pkgs

---
+++ alcaide/home/profiles/graphical.nix
@@ -0,0 +1,15 @@
+{ lib, config, ... }:
+{
+  options.alcaide.profiles.graphical.enable = lib.mkEnableOption "graphical profile";
+
+  config = lib.mkIf config.alcaide.profiles.graphical.enable {
+    alcaide = {
+      profiles.minimal.enable = true;
+      applications = {
+        applications-pkgs.enable = true;
+        vscodium.enable = true;
+      };
+      style.catppuccin.enable = true;
+  };
+};
+}
