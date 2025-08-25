d05d8ea6cc00ff016485e7fb0b0349ded633fb56
Salty4life <salty4life.634@proton.me>
2025-08-25 21:37:31 +0100

    hardware: Added AMD GPU config

---
+++ alcaide/nixos/hardware/amdgpu.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+{
+  options.alcaide.hardware.amdgpu.enable = lib.mkEnableOption "amdgpu config";
+
+  config = lib.mkIf config.alcaide.hardware.amdgpu.enable {
+    services.xserver.videoDrivers = [ "amdgpu" ];
+
+    # Enables the kernel modules for the graphics
+    hardware = {
+      graphics.enable = true;
+    };
+
+    # GUI tools
+    systemd.packages = with pkgs; [
+      lact
+    ];
+    systemd.services.lactd.wantedBy = [ "multi-user.target" ];
+  };
+}
