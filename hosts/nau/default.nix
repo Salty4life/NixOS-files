3608ae9ddf3dbec75ecb7dafcc7d2976dae09aca
Salty4life <salty4life.634@proton.me>
2025-11-17 15:17:14 +0000

    nau: added cachix outseife the flake for testing purposes

--- hosts/nau/default.nix
+++ hosts/nau/default.nix
@@ -5,6 +5,16 @@
   ...
 }:
 {
+
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   imports = [
     # keep-sorted start
     inputs.nixos-hardware.nixosModules.common-cpu-amd
