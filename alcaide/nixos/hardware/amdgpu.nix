b08e8eb871580644356029ac8517e056d6061b14
Salty4life <salty4life.634@proton.me>
2026-01-27 10:14:37 +0000

    amdgpu: Added extra util applications

--- alcaide/nixos/hardware/amdgpu.nix
+++ alcaide/nixos/hardware/amdgpu.nix
@@ -8,6 +8,8 @@
   options.alcaide.hardware.amdgpu.enable = lib.mkEnableOption "amdgpu config";
 
   config = lib.mkIf config.alcaide.hardware.amdgpu.enable {
+    boot.initrd.kernelModules = [ "amdgpu" ];
+    boot.kernelModules = [ "amdgpu" ];
     services.xserver.videoDrivers = [ "amdgpu" ];
 
     # Enables the kernel modules for the graphics
@@ -28,14 +30,17 @@
     };
 
     environment.systemPackages = with pkgs; [
+      glxinfo
+      vulkan-tools
       clinfo
+      libva-utils
       lact
+      amdgpu_top
+      nvtopPackages.amd # (h)top like task monitor for AMD, Adreno, Intel and NVIDIA GPUs
     ];
 
-    # GUI tools
-    systemd.packages = with pkgs; [
-      lact
-    ];
+    # LACT daemon activation
+    systemd.packages = with pkgs; [ lact ];
     systemd.services.lactd.wantedBy = [ "multi-user.target" ];
   };
 }
