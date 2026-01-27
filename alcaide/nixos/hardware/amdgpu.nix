02c797b236ba420f02c34b033b84ae80edec55d8
Salty4life <salty4life.634@proton.me>
2026-01-27 16:00:25 +0000

    amdgpu: Formating

--- alcaide/nixos/hardware/amdgpu.nix
+++ alcaide/nixos/hardware/amdgpu.nix
@@ -8,8 +8,10 @@
   options.alcaide.hardware.amdgpu.enable = lib.mkEnableOption "amdgpu config";
 
   config = lib.mkIf config.alcaide.hardware.amdgpu.enable {
-    boot.initrd.kernelModules = [ "amdgpu" ];
-    boot.kernelModules = [ "amdgpu" ];
+    boot = {
+      initrd.kernelModules = [ "amdgpu" ];
+      kernelModules = [ "amdgpu" ];
+    };
     services.xserver.videoDrivers = [ "amdgpu" ];
 
     # Enables the kernel modules for the graphics
