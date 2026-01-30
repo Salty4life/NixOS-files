4bf46e91bb7985cf0aa473e3d5a0ea10e0e92825
Salty4life <salty4life.634@proton.me>
2026-01-30 19:56:04 +0000

    padort: Enabled blender

--- alcaide/home/users/salty/salty@padort.nix
+++ alcaide/home/users/salty/salty@padort.nix
@@ -12,6 +12,9 @@ lib.mkIf (username == "salty" && hostName == "padort") {
   alcaide = {
     profiles.graphical.enable = true;
     games.xr.enable = true;
-    applications.krita.enable = true;
+    applications = {
+      blender.enable = true;
+      krita.enable = true;
+    };
   };
 }
